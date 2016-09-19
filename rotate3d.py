422a3bf438e9ee96aa35a3f21fbc5b5bd5a33d8e
Ben Thomas <fearlessleaderthomas@gmail.com>
2016-09-18 20:21:34 -0400

    Add basic docstrings

--- rotate3d.py
+++ rotate3d.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 
 class Object3d:
-	"""represents a 3d object"""
+	"""represents a 3d object parsed from a .obj file"""
 	def __init__(self, g, v, vn, f):
 		self.g = g
 		self.v = v
@@ -11,10 +11,12 @@ class Object3d:
 		self.f = f
 
 	def rotate(self, rmatrix):
+		"""Rotate this object by the provided rotation matrix"""
 		self.v = self.v.dot(rmatrix)
 		self.vn = self.vn.dot(rmatrix)
 
 	def write_to_file(self, filename):
+		"""Write the current state of this object to a file in .obj format."""
 		file = open(filename, 'w')
 		file.write('g {}\n'.format(self.g))
 		write_section(file, 'v', self.v)
@@ -22,11 +24,14 @@ class Object3d:
 		write_section(file, 'f', self.f)
 
 def write_section(f, identifier, array):
+	"""Write the identifier and points from array to file f."""
 	f.write('\n')
 	for item in array:
 		f.write('{} {} {} {}\n'.format(identifier, item[0], item[1], item[2]))
 
 def read_obj(filename):
+	"""Open the provided file and parse the contents as an .obj file. 
+	Return the parsed object."""
 	file = open(filename, 'r')
 	g = ''
 	v = []
@@ -45,12 +50,17 @@ def read_obj(filename):
 				f.append(tokens[1:4])
 	return Object3d(g, np.array(v, float), np.array(vn, float), np.array(f, int))
 
-
 def normalize(x,y,z):
+	"""Normalize the given 3d vector."""
 	length = (x*x + y*y + z*z)**0.5
 	return (x/length, y/length, z/length)
 
 def axis_angle_to_rotation_matrix(x, y, z, angle):
+	"""Convert the provided vector and angle to a rotation matrix and return it.
+
+	x,y,z -- the axis vector
+	angle -- the angle (in radians) to rotate around the given axis
+	"""
 	(x,y,z) = normalize(x,y,z)
 	first = math.cos(angle) * np.eye(3)
 	second = (1 - math.cos(angle)) * np.array([[x*x, x*y, x*z],[y*x,y*y,y*z],[z*x,z*y,z*z]])
@@ -58,6 +68,8 @@ def axis_angle_to_rotation_matrix(x, y, z, angle):
 	return first + second + third
 
 def main():
+	"""Load 3d object from file (default teapot.obj), rotate it 90 degrees around the Z axis, 
+	and write the result to a new file."""
 	inputfilename = "teapot.obj"
 	if len(sys.argv) > 1:
 		inputfilename = sys.argv[1]
